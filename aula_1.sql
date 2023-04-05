0223fe3ef0611743a07243e19a3e865092262454
Andrieli <62841828+strawndri@users.noreply.github.com>
2023-04-05 17:34:19 -0300

    Atualizando script da aula 1

--- aula_1.sql
+++ aula_1.sql
@@ -3,15 +3,15 @@ CREATE DATABASE ALURA;
 
 -- Criação da tabela aluno
 CREATE TABLE aluno (
-	id SERIAL,
-	nome VARCHAR(255),
-	cpf CHAR(11),
-	observacao TEXT,
-	idade INTEGER,
-	dinheiro NUMERIC(10,2),
-	altura REAL,
-	ativo BOOLEAN,
-	data_nascimento DATE,
-	hora_aula TIME,
-	data_matricula TIMESTAMP
+   id SERIAL,
+   nome VARCHAR(255),
+   cpf CHAR(11),
+   observacao TEXT,
+   idade INTEGER,
+   dinheiro NUMERIC(10,2),
+   altura REAL,
+   ativo BOOLEAN,
+   data_nascimento DATE,
+   hora_aula TIME,
+   data_matricula TIMESTAMP
 );
