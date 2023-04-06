f746d1febda20fb27c66f0941344ed9e491a2cc0
Andrieli <andrieliluci@gmail.com>
2023-04-06 13:05:22 -0300

    adicionando arquivo da aula 1

--- curso_01/aula_5.sql
+++
@@ -1,9 +0,0 @@
--- Usando CASCADE
-ALTER TABLE curso_aluno DROP id_aluno;
-
-ALTER TABLE curso_aluno ADD COLUMN id_aluno SERIAL NOT NULL;
-
-ALTER TABLE curso_aluno ADD FOREIGN KEY (id_aluno)
-REFERENCES aluno(id) 
-ON UPDATE CASCADE
-ON DELETE CASCADE;
